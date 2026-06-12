ce8f32ec9ea89ced9d3dcd72a32842ad8d7b7aec
didik Setiawan <63942553+didiksetia1@users.noreply.github.com>
2026-06-12 20:54:27 +0000

    fix: show BEM name in response title on admin aspirasi page

--- resources/views/admin/aspirasi/index.blade.php
+++ resources/views/admin/aspirasi/index.blade.php
@@ -390,7 +390,7 @@
 
             @if($aspirasi->bem_response)
             <div class="bem-response-section">
-                <div class="bem-response-title">Response BEM:</div>
+                <div class="bem-response-title">Response BEM ({{ Auth::user()->name }}):</div>
                 {{ Str::limit($aspirasi->bem_response, 150) }}
                 @if(strlen($aspirasi->bem_response) > 150) ... @endif
             </div>
